a40404b5014f8476966970e84b7755623b3ad6dd
ppezzilli <patrizio.pezzilli@objectway.com>
2019-06-07 22:57:52 +0200

    Initial Commit

---
+++ Bolletta/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Bolletta
+//
+//  Created by Patrizio on 07/06/2019.
+//  Copyright © 2019 Patrizio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Bolletta/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Bolletta
+//
+//  Created by Patrizio on 07/06/2019.
+//  Copyright © 2019 Patrizio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
